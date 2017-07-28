a805dffd58846f9adc16fb44ea5414e4ccafe3bc
Gabriel Natucci <gabrielnatucci@gmail.com>
2017-07-28 20:58:53 -0300

    Refactors webpack to include a more general loader, for both frontend and backend applications

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,72 +1,117 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
 
-module.exports = {
+module.exports = [
+  // Server configuration
+  {
+
     devtool: 'eval-source-map',
-    entry: [
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-      <% if( appType !== "Backend" ) {%>
-        'react-hot-loader/patch',
-        path.join(__dirname, 'app/index.js')
-      <%}%>
-    ],
+    entry: {
+      server: [
+        'babel-polyfill',
+        path.join(__dirname, 'server.js')
+      ]
+    },
+    target: 'node',
+    externals: [nodeExternals()],
     output: {
-        path: path.join(__dirname, '/dist/'),
-        filename: '[name].js',
-        publicPath: '/'
+      path: path.join(__dirname, '/dist/'),
+      filename: '[name].js',
+      publicPath: '/',
+      libraryTarget: 'commonjs2'
     },
     plugins: [
-      <% if( appType !== "Backend" ) {%>
-        new HtmlWebpackPlugin({
-          template: 'app/public/index.tpl.html',
-          inject: 'body',
-          filename: 'index.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-      <%}%>
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.NoErrorsPlugin(),
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('development'),
-          'process.env.APP_NAME': JSON.stringify("<%= appName %>"),
-          'process.env.APP_PORT': 3000,
-          'process.env.APP_HOST': 'http://localhost',
-          'process.env.LOG_FILENAME': JSON.stringify('logger_data')
-        })
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development'), 'process.env.APP_NAME': JSON.stringify('<%= appName %>'), 'process.env.APP_PORT': 3000, 'process.env.APP_HOST': JSON.stringify('localhost'), 'process.env.LOG_FILENAME': JSON.stringify('logger_data')})
     ],
     eslint: {
-        configFile: '.eslintrc',
-        failOnWarning: false,
-        failOnError: false
+      configFile: '.eslintrc',
+      failOnWarning: false,
+      failOnError: false
     },
     module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'eslint'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            },
-            {
-                test: /\.json?$/,
-                loader: 'json'
-            },
-            {
-                test: /\.scss$/,
-                loader: 'style!css!sass?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-            },
-            { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
-        ]
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }, {
+          test: /\.json?$/,
+          loader: 'json'
+        }, {
+          test: /\.scss$/,
+          loader: 'style!css!sass?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+        }, {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        }, {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader'
+        }
+      ]
     }
-};
+  },
+  <% if (appType !== 'Backend'){ %>
+  // Client configuration
+  {
+    devtool: 'eval-source-map',
+    entry: {
+      client: [
+        'babel-polyfill',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        'react-hot-loader/patch',
+        path.join(__dirname, 'server.js')
+      ]
+    },
+    externals: [nodeExternals()],
+    output: {
+      path: path.join(__dirname, '/dist/'),
+      filename: '[name].js',
+      publicPath: '/',
+      libraryTarget: 'commonjs2'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({template: 'app/public/index.tpl.html', inject: 'body', filename: 'index.html'}),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('development'), 'process.env.APP_NAME': JSON.stringify('<%= appName %>'), 'process.env.APP_PORT': 3000, 'process.env.APP_HOST': JSON.stringify('localhost'), 'process.env.LOG_FILENAME': JSON.stringify('logger_data')})
+    ],
+    eslint: {
+      configFile: '.eslintrc',
+      failOnWarning: false,
+      failOnError: false
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js?$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        }, {
+          test: /\.json?$/,
+          loader: 'json'
+        }, {
+          test: /\.scss$/,
+          loader: 'style!css!sass?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+        }, {
+          test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'url-loader?limit=10000&minetype=application/font-woff'
+        }, {
+          test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+          loader: 'file-loader'
+        }
+      ]
+    }
+  }
+  <%}%>
+]
