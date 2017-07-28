ca53ac098e833c3fab97966499b38215b4828d72
Gabriel Natucci <gabrielnatucci@gmail.com>
2017-07-28 20:57:48 -0300

    Changes in the models loader and exports

--- generators/app/templates/models/index.js
+++ generators/app/templates/models/index.js
@@ -3,9 +3,14 @@
 const path = require('path')
 const fs = require('fs')
 
+var fnames = []
+
 fs.readdirSync(path.join(__dirname, '/')).forEach(function (file) {
   if (file.match(/\.js$/) !== null && file !== 'index.js') {
-    var name = file.replace('.js', '') + 'Model'
-    exports[name] = require('./' + file)
+    var name = file.replace('.js', '')
+    exports[name + 'Model'] = require('./' + file)
+    fnames.push(name)
   }
 })
+
+exports['names'] = fnames
