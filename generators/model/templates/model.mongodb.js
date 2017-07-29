0013ff04eb3adac32acb529e98703168247ca345
Gabriel Natucci <gabrielnatucci@gmail.com>
2017-07-29 16:42:57 -0300

    Minor Indentation fixes and mongoDB instantiation fixes

--- generators/model/templates/model.mongodb.js
+++ generators/model/templates/model.mongodb.js
@@ -1,31 +1,31 @@
 'use strict';
 var restful = require('node-restful')
+var mongoose = restful.mongoose
 
-module.exports = function(mongoose) {
-  return restful.model(mongoose.model("<%= modelName.charAt(0).toUpperCase() + modelName.slice(1); %>", new mongoose.Schema({
-    <% for(var item of attrConfig.split("@")) { %>
-        <% if(item.split(":").length <= 2) { %>
-            <%= item.split(":")[0] %>: <%= item.split(":")[1] %>,
-            <% } else {%>
-                <%= item.split(":")[0] %>:{
-                  type: <%= item.split(":")[1] %>,
-                  <% if( item.split(":")[3].toString() == "required" ) { %>
-                  required: true,
-                  <%} else {%>
-                  required: false,
-                  <%}%>
-                  <% if( item.split(":")[1].toString() === "String") {%>
-                  default: "<%= item.split(":")[2]%>"
-                  <% } else { %>
-                  default:  <%= item.split(":")[2] %>
-                  <% } %>
-                },
-                <% } %>
-        <% } %>
-    // Sets default version of the database, for API handling purposes
-    db_version: {type: String, default: "v1.0"}
+var Model = restful.model("<%= modelName.charAt(0).toUpperCase() + modelName.slice(1); %>", new mongoose.Schema({
+<% for(var item of attrConfig.split("@")) { %>
+<% if(item.split(":").length <= 2) { %>
+    <%= item.split(":")[0] %>: <%= item.split(":")[1] %>,
+<% } else {%>
+    <%= item.split(":")[0] %>:{
+      type: <%= item.split(":")[1] %>,
+<% if( item.split(":")[3].toString() == "required" ) { %>
+      required: true,
+<%} else {%>
+      required: false,
+<%}%>
+<% if( item.split(":")[1].toString() === "String") {%>
+      default: "<%= item.split(":")[2]%>"
+<% } else { %>
+      default:  <%= item.split(":")[2] %>
+<% } %>
+  },
+<% } %><% } %>
+  // Sets default version of the database, for API handling purposes
+  db_version: {type: String, default: "v1.0"}
   },
     {timestamps: true}
-  )))
+  ))
   .methods(['get', 'post', 'put', 'delete'])
-}
+
+module.exports = Model
